9f948c48caf9b4e6950c728710381dfa930f9235
imMatheus <matheusmendesbarata03@gmail.com>
2022-10-15 14:51:30 +0200

    added tailwind

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -2,12 +2,13 @@
     import axios from 'axios'
     axios.defaults.baseURL = 'http://localhost:4000'
     import Counter from './lib/Counter.svelte'
-    const promise = axios.get('/users')
+    const promise = axios.get('/games')
 </script>
 
 <div class="card">
     <Counter />
 </div>
+<h1 class="text-3xl font-bold underline text-green-500">Hello world!</h1>
 
 {#await promise}
     <!-- promise is pending -->
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,svelte}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
